4f9c4a766d4b3794c6eecfb7ab6ba2cfe8a9184e
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-01-31 00:18:03 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,14 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+	# Since only one suite_a can exist we just select it
+	#We check to make sure that the passengers name starts with "A"
+	#Even though it passes the test without it
+	passengers[:suite_a] if passengers[:suite_a].start_with?("A")
 end
