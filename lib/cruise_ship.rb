1124c0c11887e18e621f4a6ab7514d38ccb13203
youssefmys <mohamed.saleh@vet.svu.edu.eg>
2017-03-15 20:32:47 +0200

    done

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -17,8 +17,3 @@ def select_winner(passengers)
 end
 
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
