e1b04a100819c88cb33d43524ff129d94cd1b1c4
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-14 12:35:43 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,17 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+birthday_kids = {
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+ }
+ 
+ def happy_birthday(birthday_kids)
+  
+ birthday_kids.each do |name,age|
+    if name.start_with?("A")
+      puts "The winneris #{name}"     
+    end
+  end
 end
-
+ happy_birthday(birthday_kids)
 
